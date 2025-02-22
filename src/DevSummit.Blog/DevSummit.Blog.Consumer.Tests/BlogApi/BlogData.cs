5ffcc4d3f4241bbaea09532ea3541f3208b6a4a2
March Parga, Jordi <jordi.march@wolterskluwer.com>
2025-02-22 14:54:38 +0100

    Blog Consumer Tests

---
+++ src/DevSummit.Blog/DevSummit.Blog.Consumer.Tests/BlogApi/BlogData.cs
@@ -0,0 +1,63 @@
+﻿using DevSummit.Blog.Api.Domain.Entities;
+using PactNet;
+using PactNet.Matchers;
+using System.Net;
+
+namespace DevSummit.Blog.Consumer.Tests.BlogApi;
+internal static class BlogData
+{
+    private static User[] users = [
+        new User
+        {
+            Id = Guid.Parse("757d4594-79b2-480c-8fc4-ddd7061c18cb"),
+            Name = "PermitedUser",
+            Email = "PermitedUser@user.com",
+            Access = true
+        },
+        new User
+        {
+            Id = Guid.Parse("2a69e26a-d392-41b1-82e6-68b3e4a869fb"),
+            Name = "NotPermitedUser",
+            Email = "NotPermitedUser@user.com",
+            Access = false
+        }
+    ];
+
+    public static Article GetDefaultArticle() => new Article
+    {
+        Id = Guid.Parse("f3b3b3b3-3b3b-3b3b-3b3b-3b3b3b3b3b3b"),
+        Title = "Default Article",
+        Content = "Default Article Content",
+    };
+
+    public static dynamic GetDefaultArticleDto() => new
+    {
+        Title = "Default Article",
+        Content = "Default Article Content",
+    };
+
+    public static void ConfigureRequestPactBuilder(this IPactBuilderV4 pactBuilder, string userName, string giving)
+    {
+        var mailRegexPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+        pactBuilder
+            .UponReceiving("Get Users filtered by name")
+                .Given(giving)
+                .WithRequest(HttpMethod.Get, "/api/users")
+                .WithQuery("name", Match.Equality(userName))
+            .WillRespond()
+                .WithStatus(HttpStatusCode.OK)
+                .WithJsonBody(users.Where(u => u.Name == userName).Select(u => new { id = u.Id, name = Match.Type(u.Name), email = Match.Regex(u.Email, mailRegexPattern) }));
+
+        var user = users.FirstOrDefault(u => u.Name == userName);
+        if (user != null)
+        {
+            pactBuilder
+                .UponReceiving("Get User details by user id")
+                    .Given(giving)
+                    .WithRequest(HttpMethod.Get, $"/api/users/{user.Id}")
+                .WillRespond()
+                    .WithStatus(HttpStatusCode.OK)
+                    .WithJsonBody(new { name = Match.Type(user.Name), email = Match.Regex(user.Email, mailRegexPattern), hasAccess = user.Access });
+        }
+    }
+}
---
+++ src/DevSummit.Blog/DevSummit.Blog.Consumer.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,42 @@
+﻿using DevSummit.Blog.Api.Domain.Entities;
+using DevSummit.Blog.Api.Domain.Services;
+using DevSummit.Blog.Api.Domain.Repositories;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevSummit.Blog.Consumer.Tests;
+
+public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+    }
+
+    public HttpClient CreateClient(string urlPermissions)
+    {
+        Environment.SetEnvironmentVariable("USERS_PERMISSIONS_API_URL", urlPermissions);
+        return CreateClient();
+    }
+
+    public void SaveArticle(Article article)
+    {
+        using var scope = Services.CreateScope();
+        var articlesService = scope.ServiceProvider.GetRequiredService<IArticlesService>();
+        articlesService.AddArticle(article);
+    }
+
+    public void DeleteArticle(Guid id)
+    {
+        using var scope = Services.CreateScope();
+        var articlesService = scope.ServiceProvider.GetRequiredService<IArticlesService>();
+        articlesService.DeleteArticle(id);
+    }
+
+    public Article? GetArticleById(Guid id)
+    {
+        using var scope = Services.CreateScope();
+        var repository = scope.ServiceProvider.GetRequiredService<IArticlesRepository>();
+        return repository.GetById(id);
+    }
+}
