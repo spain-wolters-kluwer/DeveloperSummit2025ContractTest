0b599861f8eb7956ea295208956f4a5b564a6ebd
March Parga, Jordi <jordi.march@wolterskluwer.com>
2025-02-26 10:06:13 +0100

    Test can I deploy

---
+++ scripts/canIdeploy.ps1
@@ -0,0 +1,28 @@
+param (
+    [string]$pactUrl,
+    [string]$pacticipant,
+    [string]$version,
+    [string]$environment,
+    [string]$username,
+    [string]$password
+)
+
+$url = "$pactUrl/can-i-deploy?pacticipant=$pacticipant&version=$version&environment=$environment"
+
+$headers = @{
+    "Content-Type" = "application/json"
+    "Accept" = "application/hal+json, application/json, */*; q=0.01"
+    "X-Interface" = "HAL Browser"
+    "Authorization" = "Basic " + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("${username}:${password}"))
+}
+
+$response = Invoke-RestMethod -Uri $url -Method Get -Headers $headers
+if ($response.summary.deployable -eq "true") {
+    Write-Output "Success: "$response.summary.reason
+    exit 0
+} else {
+    Write-Output "Fail: "$response.summary.reason
+    exit 1
+}
+
+
