d95213ffffa56c63d6ccec73e24daa46a0685c5d
March Parga, Jordi <jordi.march@wolterskluwer.com>
2025-02-27 14:20:37 +0100

    Set final state

--- src/DevSummit.Blog/DevSummit.Blog.Consumer.Tests/BlogApi/BlogData.cs
+++ src/DevSummit.Blog/DevSummit.Blog.Consumer.Tests/BlogApi/BlogData.cs
@@ -9,8 +9,7 @@ internal static class BlogData
     private static User[] users = [
         new User
         {
-            //Id = Guid.Parse("62d7a17a-6273-4863-bc5f-2e096e81e749"),
-            Id = Guid.NewGuid(),
+            Id = Guid.Parse("62d7a17a-6273-4863-bc5f-2e096e81e749"),
             Name = "ReadOnlyUser",
             Email = "ReadOnlyUser@user.com",
             Role = UserRoles.ReadOnly
