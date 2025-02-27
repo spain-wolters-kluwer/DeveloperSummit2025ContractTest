1a55825c2304d13d749336e2eb664b3c5b9fdb85
March Parga, Jordi <jordi.march@wolterskluwer.com>
2025-02-27 13:23:59 +0100

    Force pipeline

--- src/DevSummit.Blog/DevSummit.Blog.Consumer.Tests/BlogApi/BlogData.cs
+++ src/DevSummit.Blog/DevSummit.Blog.Consumer.Tests/BlogApi/BlogData.cs
@@ -22,6 +22,7 @@ internal static class BlogData
             Name = "FullAccessUser",
             Email = "FullAccessUser@user.com",
             Role = UserRoles.FullAccess
+
         },
         new User
         {
